97d21a5a6d5deda8250013639142a11f8d4a377e
JulesVan <javanwyhe@gmail.com>
2014-02-13 17:31:23 -0800

    very slightly more Rubocop friendly

--- txt2csv/lib/parse_names.rb
+++ txt2csv/lib/parse_names.rb
@@ -1,12 +1,10 @@
 class Parse
-
   def self.parse_names(prefixes, suffixes, name_string)
     parsed_name = { pre: '', first: '', middle: '', last: '', suffix: '' }
 
     word = name_string.split
     parsed_name[:suffix] = word.pop if suffixes.include? word.last
     parsed_name[:last] = word.last
-
     parsed_name[:pre] = word.shift if prefixes.include? word.first
     parsed_name[:first] = word.shift if word.count > 1
     parsed_name[:middle] = word.first if word.count > 1
